523166d2f80c3f55bd51eea7b688e6daa3c72474
Andrey Gershun <agershun@gmail.com>
2016-08-20 06:15:38 +0300

    READE.me changes

--- node-red-contrib-ala-file.js
+++ node-red-contrib-ala-file.js
@@ -24,7 +24,6 @@ module.exports = function(RED) {
 			var sql = 'SELECT '+this.columns
 					+' INTO '+this.format+'("'+doubleb(this.filename)
 					+'",{headers:'+this.headers+'}) FROM ?';
-			console.log(sql);
 			var bind = Array.isArray(msg.payload) ? [msg.payload] : [[msg.payload]];
 			alasql.promise(sql, bind)
 				.then(function(res){
@@ -37,4 +36,28 @@ module.exports = function(RED) {
 			
 	}
 	RED.nodes.registerType("ala-file out",AlaFileNodeOut);
+
+	function AlaFileNode(config) {
+		RED.nodes.createNode(this,config);
+        var node = this;
+        node.filename = config.filename;
+        node.format = config.format;
+        node.columns = config.columns;
+        node.headers = config.headers;
+		node.on("input", function(msg) {
+			var sql = 'SELECT '+this.columns
+					+' FROM '+this.format+'("'+doubleb(this.filename)
+					+'",{headers:'+this.headers+'})';
+			var bind = Array.isArray(msg.payload) ? [msg.payload] : [[msg.payload]];
+			alasql.promise(sql, bind)
+				.then(function(res){
+					msg.payload = res;
+					node.send(msg);
+				}).catch(function(err){
+					node.error(err,msg);
+				});
+		});
+			
+	}
+	RED.nodes.registerType("ala-file",AlaFileNode);
 }
