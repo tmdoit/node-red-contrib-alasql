ea251fc87ae60ce964a6fc77ceb36ca1234e27ca
PotOfCoffee2Go <kim@lrunit.net>
2017-06-22 18:39:04 -0400

    Sync days of local changes with remote

--- node-red-contrib-ala-file.js
+++ node-red-contrib-ala-file.js
@@ -21,19 +21,34 @@ module.exports = function(RED) {
         node.columns = config.columns;
         node.headers = config.headers;
 		node.on("input", function(msg) {
-			var sql = 'SELECT '+this.columns
-					+' INTO '+this.format+'("'+doubleb(this.filename)
-					+'",{headers:'+this.headers+'}) FROM ?';
-			var bind = Array.isArray(msg.payload) ? [msg.payload] : [[msg.payload]];
-			alasql.promise(sql, bind)
-				.then(function(res){
-					msg.payload = res;
-					node.send(msg);
-				}).catch(function(err){
-					node.error(err,msg);
-				});
+            var filename = node.filename || msg.filename || "";
+            if (filename !== "") {
+                node.status({fill:"grey",shape:"dot",text:filename});
+            }
+            if (filename === "") {
+                node.warn(RED._("file.errors.nofilename"));
+				node.status({fill:"red",shape:"ring",text:"No filename in node config or msg.filename!"});
+            }
+            else if (msg.hasOwnProperty("payload") && (typeof msg.payload !== "undefined")) {
+				var sql = 'SELECT '+this.columns
+						+' INTO '+this.format+'("'+doubleb(filename)
+						+'",{headers:'+this.headers+'}) FROM ?';
+				var bind = Array.isArray(msg.payload) ? [msg.payload] : [[msg.payload]];
+				alasql.promise(sql, bind)
+					.then(function(res){
+						msg.payload = res;
+						node.send(msg);
+					}).catch(function(err){
+						node.error(err,msg);
+					});
+			}
+			else {
+				node.status({fill:"red",shape:"ring",text:"No msg.payload defined with data to output!"});
+			}
 		});
-			
+        this.on('close', function() {
+            node.status({});
+        });
 	}
 	RED.nodes.registerType("ala-file out",AlaFileNodeOut);
 
@@ -45,19 +60,31 @@ module.exports = function(RED) {
         node.columns = config.columns;
         node.headers = config.headers;
 		node.on("input", function(msg) {
-			var sql = 'SELECT '+this.columns
-					+' FROM '+this.format+'("'+doubleb(this.filename)+'.'+this.format
-					+'",{headers:'+this.headers+'})';
-			var bind = Array.isArray(msg.payload) ? [msg.payload] : [[msg.payload]];
-			alasql.promise(sql, bind)
-				.then(function(res){
-					msg.payload = res;
-					node.send(msg);
-				}).catch(function(err){
-					node.error(err,msg);
-				});
+            var filename = node.filename || msg.filename || "";
+            if (filename !== "") {
+                node.status({fill:"grey",shape:"dot",text:filename});
+            }
+            if (filename === "") {
+                node.warn(RED._("file.errors.nofilename"));
+				node.status({fill:"red",shape:"ring",text:"No filename in node config or msg.filename!"});
+            }
+            else if (msg.hasOwnProperty("payload") && (typeof msg.payload !== "undefined")) {
+				var sql = 'SELECT '+this.columns
+						+' FROM '+this.format+'("'+doubleb(filename)+'.'+this.format
+						+'",{headers:'+this.headers+'})';
+				var bind = Array.isArray(msg.payload) ? [msg.payload] : [[msg.payload]];
+				alasql.promise(sql, bind)
+					.then(function(res){
+						msg.payload = res;
+						node.send(msg);
+					}).catch(function(err){
+						node.error(err,msg);
+					});
+			}
 		});
-			
+        this.on('close', function() {
+            node.status({});
+        });
 	}
-	RED.nodes.registerType("ala-file",AlaFileNode);
+	RED.nodes.registerType("ala-file in",AlaFileNode);
 }
